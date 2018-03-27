9f8f42ea77306627fd660cc31e731fed17974419
Tom Womack <tom@womack.net>
2018-03-27 08:34:28 +0100

    Replace error lines by a count of errors

---
+++ shorten-msieve.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl -w
+use strict;
+my $ect=0; my $line;
+while (<>)
+{
+    if (/error -([0-9]+) reading relation ([0-9]+)/) { $ect++; }
+    else { if ($ect==0) { print } else {print "  $ect errors\n"; $ect=0; } }
+}
