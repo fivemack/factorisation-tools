c174e0f7c12c39d94c981fd5abc3f2f5d5c72ca1
Tom Womack <tom@womack.net>
2018-04-22 18:11:07 +0100

    Trivial tool for updating out-of-date .gpl and .ath files

---
+++ ud.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+my @x = glob("[0-9]*[0-9][0-9]");
+for my $u (@x)
+{
+    my $age1 = -M $u;
+    my $age2 = -M "$u.gpl";
+    if ($age1 < $age2)
+    {
+	system "python for_factorization.ath.cx.py $u";
+    }
+}
