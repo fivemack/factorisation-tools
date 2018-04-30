61972af81a21b45d37128879c8cbde8f27797cb7
Tom Womack <tom@womack.net>
2018-04-30 08:06:57 +0100

    Tools for parsing the nfs@home results page

---
+++ nfshome-graph.pl
@@ -0,0 +1,151 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $colct;
+
+for my $toplevel ("d","e")
+{
+    my $output;
+    my %ocols; my $next_ocol = 0;
+    my @projnames; $projnames[0]="Unix time";
+    
+    my @efiles = glob("$toplevel.*");
+    for my $f (@efiles)
+    {
+	my $oline;
+	$f =~ m/[de]\.([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})/;
+	my ($Y,$M,$D,$h)=($1,$2,$3,$4);
+	my @stat = stat $f; my $mt = $stat[9];
+#    print "$f $mt\n";
+	
+	my $content;
+	{
+	    local $/ = undef;
+	    open A,"< $f";
+	    $content = <A>;
+	    close A;
+	}
+	$content =~ m#Now sieving</h2>.*(<table .*</table>)#;
+	my $sievetab = $1;
+	my @rows = split "<tr>",$sievetab;
+	for my $j (0..$#rows)
+	{
+	    $rows[$j] =~ s#</(table|tr)>##g;
+	    my @cols = split "<td[^>]*>",$rows[$j];
+	    for my $i (0..$#cols)
+	    {
+		$cols[$i] =~ s#</td>##g;
+#	    print "$j $i $cols[$i]\n";
+	    }
+	    if ($j>=2)
+	    {
+		my ($project,$rels)=($cols[2],$cols[11]);
+		$rels =~ s/,//g;
+		if ($rels > 0)
+		{
+#	    print $mt," ",$cols[2]," ",$cols[11],"\n";
+		    if (! defined $ocols{$project})
+		    {
+			$ocols{$project} = $next_ocol++;
+			$projnames[$next_ocol]=$project;
+		    }
+		    $oline->[$ocols{$project}]=$rels;
+		}
+	    }
+	}
+	$output->{$mt}=$oline;
+    }
+
+# rewrite project names to come out right in gnuplot
+    for my $pn (@projnames)
+    {
+	if ($pn =~ m/([0-9]*)_([0-9]*)_minus1/) { $pn = "$1^$2-1"; }
+	elsif ($pn =~ m/GW_([0-9]*)_([0-9]*)/) { $pn = "$2*$1^$2-1"; }
+	elsif ($pn =~ m/GC_([0-9]*)_([0-9]*)/) { $pn = "$2*$1^$2+1"; }
+	elsif ($pn =~ m/C([0-9]*)_([0-9]*)_([0-9]*)/) { $pn = "C$1 from $2^$3+$3^$2"; }
+	elsif ($pn =~ m/W_([0-9]*)/) { $pn = "$1*2^$1-1"; }
+	elsif ($pn =~ m/C_([0-9]*)/) { $pn = "$1*2^$1+1"; }
+	elsif ($pn =~ m/([0-9]{5})_([0-9]*)/)
+	{
+	    if (substr($1,2,1) eq substr($1,1,1))
+	    {
+		$pn = "near-repdigit $2 digits pattern $1";
+	    }
+	    else
+	    {
+		$pn = "near-repdigit ".(2*$2+1)." digits pattern $1";
+	    }
+	}
+
+	my $upn = ""; my $ss = 0;
+	for my $ci (0..length($pn))
+	{
+	    my $c = substr($pn,$ci,1);
+	    if ($ss == 1 && !($c =~ m/[0-9]/)) { $ss=0; }
+	    if ($c eq "^") { $ss = 1; }
+	    else
+	    {
+		if ($ss==1) { $upn .= "^"; }
+		$upn .= $c;
+	    }
+	}
+	$pn=$upn;
+    }
+    
+    open AUS,"> parse-$toplevel.aus";
+    print AUS join ",",@projnames,"\n";
+    my $num_ocols = $next_ocol-1;
+    my $firstline = 1; my @prev; my $t0 = -1;
+    for my $t (sort {$a <=> $b} keys %{$output})
+    {
+	if ($firstline != 1 && defined @{$output->{$t}})
+	{
+	    my @delta = @{$output->{$t}};
+	    for my $v (0..$num_ocols)
+	    {
+		if (!defined $delta[$v]) { $delta[$v] = 0; }
+		if (defined $prev[$v])
+		{
+		    if ($delta[$v] != 0)
+		    {
+			$delta[$v] -= $prev[$v];
+		    }
+		}
+		$delta[$v] /= ($t-$t0);
+	    }
+	    print AUS $t,",",join ",",@delta,"\n";
+	}
+	@prev = @{$output->{$t}}; $t0 = $t;
+	$firstline = 0;
+    }
+    close AUS;
+# and generate the graph file, because they need to know the count
+# of entries on the line
+# but count the timestamp, and gnuplot starts counting at 1
+    $num_ocols += 2;
+    open GRAPH,"> graph-$toplevel.gpl";
+    print GRAPH <<END;
+set title "Relations per second"
+set xdata time
+set timefmt "%s"
+set datafile separator ","
+set terminal png size 1600,1200 enhanced truecolor font 'Verdana,9'
+set output "RPS-$toplevel.png"
+set ylabel "Relations per second"
+set xlabel "Date"
+set yrange [0:10<*]
+set pointsize 0.8
+set format x "%d/%m"
+set border 11
+set xtics out
+set tics front
+set key below
+plot \\
+  for [i=2:$num_ocols:1] \\
+    "parse-$toplevel.aus" using 1:(sum [col=i:$num_ocols] column(col)) \\
+      title columnheader(i) \\
+      with filledcurves x1 lc (i-1)\%6
+END
+    close GRAPH;
+}
+
---
+++ nfshome-progress.pl
@@ -0,0 +1,120 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $colct;
+my @resultat;
+
+sub commify($)
+{
+    my ($n) = @_;
+    my $n0 = $n;
+    my $m = "";
+    while (length($n)>3)
+    {
+	$m = ",".substr($n,-3,3).$m;
+	$n = substr($n,0,-3);
+    }
+    $m = $n.$m;
+    return $m;
+}
+
+for my $toplevel ("d","e")
+{
+    my @projnames; $projnames[0]="Unix time";
+
+    my $output;
+    my %ocols; my $next_ocol = 0;
+    
+    my @efiles = glob("$toplevel.*");
+    for my $f (@efiles)
+    {
+	my $oline;
+	$f =~ m/[de]\.([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})/;
+	my ($Y,$M,$D,$h)=($1,$2,$3,$4);
+	my @stat = stat $f; my $mt = $stat[9];
+	
+	my $content;
+	{
+	    local $/ = undef;
+	    open A,"< $f";
+	    $content = <A>;
+	    close A;
+	}
+	$content =~ m#Now sieving</h2>.*(<table .*</table>)#;
+	my $sievetab = $1;
+	my @rows = split "<tr>",$sievetab;
+	for my $j (0..$#rows)
+	{
+	    $rows[$j] =~ s#</(table|tr)>##g;
+	    my @cols = split "<td[^>]*>",$rows[$j];
+	    for my $i (0..$#cols)
+	    {
+		$cols[$i] =~ s#</td>##g;
+#	    print "$j $i $cols[$i]\n";
+	    }
+	    if ($j>=2)
+	    {
+		my ($project,$rels)=($cols[2],$cols[11]);
+		$rels =~ s/,//g;
+		if ($rels > 0)
+		{
+#	    print $mt," ",$cols[2]," ",$cols[11],"\n";
+		    if (! defined $ocols{$project})
+		    {
+			$ocols{$project} = $next_ocol++;
+			$projnames[$next_ocol]=$project;
+		    }
+#		    print "ng ",$f, " ", $project," ",$rels," ",$ocols{$project},"\n";
+		    $oline->[$ocols{$project}]=$rels;
+		}
+	    }
+	}
+	$output->{$mt}=$oline;
+    }
+
+    my @toad = sort keys %{$output};
+    my $now = $output->{$toad[-1]};
+    my $then = $output->{$toad[-5]};
+    my $ncol = $next_ocol;
+    for my $u (0..$ncol)
+    {
+	if (defined $now->[$u] and defined $then->[$u])
+	{
+	    my $progress;
+	    if ($now->[$u] == $then->[$u]) { $progress = "done"; }
+	    else { $progress = "+".sprintf("%6.2g",($now->[$u] - $then->[$u])); }
+	    $progress =~ s/ //g;
+	    my $pn = $projnames[$u+1];
+	    $pn =~ s/ /_/g;
+	    push @resultat,["[".$toplevel."]", $pn, commify($now->[$u]), $progress];
+	}
+    }
+}
+
+my @rcw;
+for my $u (0..3)
+{
+    for my $q (@resultat)
+    {
+	my $l = length($q->[$u]);
+	if ($l > $rcw[$u]) { $rcw[$u]=$l; }
+    }
+}
+
+for my $q (@resultat)
+{
+    for my $u (0..3)
+    {
+	my $l = length($q->[$u]);
+	my $j = " "x($rcw[$u]-$l);
+	if ($u != 2)
+	{
+	    print $q->[$u],$j," ";
+	}
+	else
+	{
+	    print $j,$q->[$u]," ";
+	}
+    }
+    print "\n";
+}
