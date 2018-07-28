1dcaab47e705b4ba5c4667b5bb3cf8b134e4c620
Tom Womack <tom@womack.net>
2018-07-29 00:12:28 +0100

    Report number of errors even if log ends with an error

--- shorten-msieve.pl
+++ shorten-msieve.pl
@@ -6,3 +6,7 @@ while (<>)
     if (/error -([0-9]+) reading relation ([0-9]+)/) { $ect++; }
     else { if ($ect==0) { print } else {print "  $ect errors\n"; $ect=0; } }
 }
+if ($ect != 0)
+{
+    print "  $ect errors\n";
+}
