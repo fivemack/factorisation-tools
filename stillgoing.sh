d6b7886b990649f7f6b62d40e9dea3fcfea7121b
Tom Womack <tom@womack.net>
2018-11-11 14:56:46 +0000

    remove more useless processes from the process list in stillgoing

--- stillgoing.sh
+++ stillgoing.sh
@@ -1,1 +1,1 @@
- for u in *gpl; do [ -s $u ] && echo $u $(tail -n 2 $u | head -n 1) $(tail -n 1 $u) $(ps aux | grep aliquot | grep ${u%.gpl} | grep -iv SCREEN); done | awk '{print $5-$3,$0}' | sort -g
+for u in *gpl; do [ -s $u ] && echo $u $(tail -n 2 $u | head -n 1) $(tail -n 1 $u) $(ps aux | grep aliquot | grep ${u%.gpl} | grep -iv SCREEN | grep -v tail); done | awk '{print $5-$3,$0}' | sort -g
